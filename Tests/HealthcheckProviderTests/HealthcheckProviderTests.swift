d9a9c59c61bc49f00c4ae40e8256ca3a97b6b9fa
Vinod Madigeri <vmadigeri@apple.com>
2018-02-10 19:26:17 -0800

    Added testMissingConfigURL

--- Tests/HealthcheckProviderTests/HealthcheckProviderTests.swift
+++ Tests/HealthcheckProviderTests/HealthcheckProviderTests.swift
@@ -23,6 +23,21 @@ class HealthcheckProviderTests: XCTestCase {
             .assertStatus(is: .ok)
             .assertJSON("status", equals: "up")
     }
+    
+    func testMissingConfigURL() {
+        let config = try! Config(arguments: ["vapor", "--env=test"])
+        // Don't add the URL for this test
+        //try! config.set("healthcheck.url", "healthcheck")
+        try! config.addProvider(HealthcheckProvider.Provider.self)
+        let drop = try! Droplet(config)
+        background {
+            try! drop.run()
+        }
+        
+        try! drop
+            .testResponse(to: .get, at: "healthcheck")
+            .assertStatus(is: .notFound)
+    }
 
     static var allTests = [
         ("testHealthcheck", testHealthcheck),
